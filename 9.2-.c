1724376393f6d0dd08ae13ee76319f46a24b6e86
Richard Shaji Mekkaden <richardshajimekkaden@Richards-MacBook-Air.local>
2026-03-02 01:05:45 +0530

    fixed priorit

---
+++ 9.2-.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <limits.h>
+
+struct process {
+    int pid, at, bt, pr, ct, tat, wt, done;
+};
+
+int main() {
+    int n;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    struct process p[n];
+
+    for(int i = 0; i < n; i++) {
+        printf("Enter AT BT PR of P%d: ", i+1);
+        scanf("%d %d %d", &p[i].at, &p[i].bt, &p[i].pr);
+        p[i].pid = i + 1;
+        p[i].done = 0;
+    }
+
+    int completed = 0, time = 0;
+    float sumWT = 0, sumTAT = 0;
+
+    while(completed < n) {
+        int idx = -1, max_pr = -1;
+
+        for(int i = 0; i < n; i++) {
+            if(p[i].at <= time && p[i].done == 0) {
+                if(p[i].pr > max_pr) {
+                    max_pr = p[i].pr;
+                    idx = i;
+                }
+            }
+        }
+
+        if(idx == -1) {
+            time++;
+            continue;
+        }
+
+        time += p[idx].bt;
+        p[idx].ct = time;
+        p[idx].tat = p[idx].ct - p[idx].at;
+        p[idx].wt = p[idx].tat - p[idx].bt;
+        p[idx].done = 1;
+
+        sumWT += p[idx].wt;
+        sumTAT += p[idx].tat;
+        completed++;
+    }
+
+    printf("\nPID\tAT\tBT\tPR\tCT\tTAT\tWT\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+            p[i].pid, p[i].at, p[i].bt, p[i].pr,
+            p[i].ct, p[i].tat, p[i].wt);
+    }
+
+    printf("\nAverage TAT = %.2f", sumTAT / n);
+    printf("\nAverage WT  = %.2f\n", sumWT / n);
+
+    return 0;
+}
